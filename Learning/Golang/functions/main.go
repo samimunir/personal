b9d886763d1ea7560579eab9abc7bfa286bfd59e
Sami Munir <samidmunir@outlook.com>
2025-10-21 14:17:13 -0400

    completing functions chapter in Go

--- Learning/Golang/functions/main.go
+++ Learning/Golang/functions/main.go
@@ -15,6 +15,11 @@ func main() {
 	fmt.Println("\nSum of 3 + 5 = ", add(3, 5))
 
 	fmt.Println("\nSubtraction of 10 - 4 = ", subtract(10, 4))
+
+	main_v2()
+
+	get_coords()
+	get_coords_v2()
 }
 
 func say_hello(name string) {
@@ -44,3 +49,34 @@ func subtract(a, b int) int {
 	- variables in Go are passed by value, which means that when a variable is passed into a function, that function receives a copy of the variable.
 	- the function is unable to mutate the caller's data.
 */
+
+/*
+	A function can return a value that the caller doesn't care about. We can explicitly ignore variables by using an underscore: _
+*/
+func get_points() (x int, y int) {
+	return 3, 4
+}
+
+func main_v2() {
+	x, _ := get_points()
+	fmt.Println("\nX.points:", x)
+}
+
+/*
+	A return statement without arguments returns the named return values. This is known as a "naked" return.
+	Naked return statements should be used only in short functions. They can harm readability in longer functions.
+*/
+func get_coords() (x, y int) {
+	// x and y are initialized with zero values.
+
+	return // automatically returns x and y.
+}
+/*
+	The functions above and below are equivalent.
+*/
+func get_coords_v2() (int, int) {
+	var x int
+	var y int
+
+	return x, y
+}
