d0a70a2afad093ad22b2ba8eace0eee1f1e998a5
Sami Munir <samidmunir@outlook.com>
2025-10-22 23:47:27 -0400

    setter injection for dependency injection

---
+++ Learning/Java Spring/spring_example/src/main/java/car/example/setter/injection/Specification.java
@@ -0,0 +1,19 @@
+package car.example.setter.injection;
+
+public class Specification {
+
+    private String make;
+    private String model;
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String toString() {
+        return "Specification {make = " + this.make + ", model = " + this.model + "}";
+    }
+}
