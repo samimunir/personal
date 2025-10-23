35aa300a0e0228ab0bebed4ca21874f1a6e9edff
Sami Munir <samidmunir@outlook.com>
2025-10-22 23:00:52 -0400

    integrating Spring-core into Maven proj

--- Learning/Java Spring/spring_example/src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ Learning/Java Spring/spring_example/src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -4,8 +4,13 @@ public class LooseCouplingExample {
 
     public static void main(String[] args) {
         UserDataProvider databaseProvider = new UserDatabaseProvider();
-
         UserManager userManagerWithDB = new UserManager(databaseProvider);
         System.out.println(userManagerWithDB.getUserInfo());
+
+        System.out.println("-----");
+
+        UserDataProvider webServiceProvider = new WebServiceDataProvider();
+        UserManager userManagerWithWS = new UserManager(webServiceProvider);
+        System.out.println(userManagerWithWS.getUserInfo());
     }
 }
