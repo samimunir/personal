34698b533669b070f57c351620f96e62e6a393d2
Sami Munir <samidmunir@outlook.com>
2025-10-28 17:15:14 -0400

    creating hello-post request/controller + learning PathVariable annotation

--- Learning/Java Spring/firstspring/src/main/java/com/zephiron/firstspring/HelloController.java
+++ Learning/Java Spring/firstspring/src/main/java/com/zephiron/firstspring/HelloController.java
@@ -1,13 +1,22 @@
 package com.zephiron.firstspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello world!";
+    @GetMapping("/hello/{name}")
+    public HelloResponse hello(@PathVariable String name) {
+        return new HelloResponse("Hello world, " + name + "!");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name) {
+        String greeting = "Hello world, " + name + "!";
+        return new HelloResponse(greeting);
     }
 }
