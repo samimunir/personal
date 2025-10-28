b85e3d7861fed0524730fb0565501565de0496bc
Sami Munir <samidmunir@outlook.com>
2025-10-28 19:28:59 -0400

    implementing PostMapping request to create new category

--- Learning/Java Spring/sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ Learning/Java Spring/sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
@@ -16,5 +18,11 @@ public class CategoryController {
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
         return this.categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        this.categories.add(category);
+        return "Category created successfully.";
     }
 }
