c3d2ba3239b1d6b7ab89ff5a501e2ac835da645f
Sami Munir <samidmunir@outlook.com>
2025-10-29 02:23:18 -0400

    setting up routing + layout (initial)

--- Projects/developer-portfolio/client/src/main.tsx
+++ Projects/developer-portfolio/client/src/main.tsx
@@ -1,11 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+// Add component/page imports here.
 import "./styles/globals.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
