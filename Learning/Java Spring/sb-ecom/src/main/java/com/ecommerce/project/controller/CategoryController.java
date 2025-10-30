7bea445e93be29694f3dec3323832215d0df5e4f
Sami Munir <samidmunir@outlook.com>
2025-10-30 15:13:48 -0400

    implementing ResponseEntity class object to return String msg + HttpStatusCode

--- Learning/Java Spring/sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ Learning/Java Spring/sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -2,12 +2,15 @@ package com.ecommerce.project.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
@@ -33,8 +36,12 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryID}")
-    public String deleteCategory(@PathVariable Long categoryID) {
-        String status = categoryService.deleteCategory(categoryID);
-        return status;
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryID) {
+        try {
+            String status = categoryService.deleteCategory(categoryID);
+            return new ResponseEntity<>(status, HttpStatus.OK);
+        } catch (ResponseStatusException e) {
+            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        }
     }
 }
--- Learning/Java Spring/sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ Learning/Java Spring/sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -3,7 +3,9 @@ package com.ecommerce.project.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.ecommerce.project.model.Category;
 
@@ -30,7 +32,7 @@ public class CategoryServiceImpl implements CategoryService {
             .filter(c -> c.getCategoryID()
             .equals(categoryID))
             .findFirst()
-            .orElse(null);
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         if (category == null) {
             return "Category not found. Failed to delete.";
