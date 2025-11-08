297452cefb9e28230ee7585f18e338dd4a3ccc32
Sami Munir <samidmunir@outlook.com>
2025-11-08 15:44:52 -0500

    implementing Landing + Products page + react-router-dom integration

--- Client/Top Tier Customs LLC./ttc-app/apps/web/src/App.jsx
+++ Client/Top Tier Customs LLC./ttc-app/apps/web/src/App.jsx
@@ -1,11 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+
 import Main from "./layouts/Main";
 import Landing from "./pages/Landing";
+import Products from "./pages/Products";
 
 const App = () => {
   return (
-    <Main>
-      <Landing />
-    </Main>
+    <Routes>
+      <Route
+        index
+        path="/"
+        element={
+          <Main>
+            <Landing />
+          </Main>
+        }
+      />
+      <Route
+        path="/shop"
+        element={
+          <Main>
+            <Products />
+          </Main>
+        }
+      />
+    </Routes>
   );
 };
 
